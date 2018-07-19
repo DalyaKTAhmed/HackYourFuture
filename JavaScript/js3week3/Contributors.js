6d57080f2b127ecd00adcb217f3422e53ca370dc
DalyaKTAhmed <37067524+DalyaKTAhmed@users.noreply.github.com>
2018-07-19 10:42:17 +0200

    Add files via upload

---
+++ JavaScript/js3week3/Contributors.js
@@ -0,0 +1,34 @@
+class Contributors {
+    constructor(data) {
+        this._data = data;
+    }
+
+    showContributorsInList(data) {
+
+        const list = document.getElementById("contributorList")
+
+        while (list.hasChildNodes()) {
+
+            list.removeChild(list.firstChild);
+
+        }
+        // Renders new list. 
+        contributors.forEach(contributor => {
+
+            const contributorContainer = document.createElement('div');
+            contributorContainer.setAttribute('class', 'contributorContainer');
+
+            document.body.appendChild(contributorContainer);
+
+            contributorContainer.innerHTML = `<img width="100px" src="${contributor.avatar_url}" alt="contributor image"> 
+
+                                            <p class="name"> <a href="${contributor.contributors_url}" > ${contributor.login}</p> 
+
+                                            <span class="cont"> ${contributor.contributions}</span>`;
+
+
+
+
+        })
+    }
+}
---
+++ JavaScript/js3week3/Loader.js
@@ -0,0 +1,15 @@
+window.Loader = class Loader {
+    constructor() {
+
+    }
+
+    openModal() {
+        // modalTimerId = window.setTimeout(() => { document.getElementById('modal').style.display = 'block'; }, 1000);
+        document.getElementById('modal').style.display = 'block';
+    }
+
+    closeModal() {
+        // window.clearTimeout(modalTimerId);
+        document.getElementById('modal').style.display = 'none';
+    }
+}
